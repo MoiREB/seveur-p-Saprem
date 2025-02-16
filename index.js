46f6f6ecb2a304ba4a955c658a0dea1b90af758b
MoiREB <canapaxy@gmail.com>
2025-02-16 20:07:47 +0100

    test 4

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const admin = require('firebase-admin');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
+
 /*
 // Charger les variables d'environnement
 dotenv.config();
@@ -18,22 +19,33 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-
+*/
 
 const app = express();
-const port = 3000;
-*/
+//const port = 3000;
+
+/*
 // Middleware pour CORS
 app.use(cors()); // Ajout du middleware CORS
 
 // Middleware pour parser le corps des requêtes en JSON
-//app.use(bodyParser.json());
-app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
+app.use(bodyParser.json());
+*/
 
-});
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token,Origin, X-Requested-With, Content, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
-/*
+
+  app.use((req, res) => {
+    res.json({ message: "UPDATE !" });
+ });
+
 // Endpoint pour supprimer un utilisateur
 app.post('/deleteUser', async (req, res) => {
   const { uid } = req.body;
@@ -50,7 +62,7 @@ app.post('/deleteUser', async (req, res) => {
     console.error("Erreur lors de la suppression de l'utilisateur:", error);
     res.status(500).json({ message: 'Erreur lors de la suppression de l\'utilisateur' });
   }
-});*/
+});
 
 /*app.listen(port, () => {
   console.log(`Serveur démarré sur http://localhost:${port}`);
